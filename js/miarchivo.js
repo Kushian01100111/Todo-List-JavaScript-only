554619d23321c1ee12dabe6b8a7c32ab3e55d2c1
Kushian01100111 <comaspedro6@gmail.com>
2022-08-10 21:07:12 -0300

    10-08-2022: - Optimacion general del codigo - Cambios en estilos para que la aplicacion - Añadido la opcion de poder marcar una tarea completada

--- js/miarchivo.js
+++ js/miarchivo.js
@@ -4,6 +4,21 @@ Date.prototype.addDays = function(days) {
     return date;
 }
 
+let noBarrita = (x)=>{
+    return x.split("-").join(" ")
+}
+
+let ponerBarrita = (nombre)=>{
+    return  nombre.split(" ").join("-")
+} 
+
+let tareaRemovida = (cate,nom) =>{
+    swal({
+        title:`Tu tarea fue eliminada con exito`,
+        text: `La tarea ${noBarrita(nom)} fue eliminada de la categoria ${cate}`,
+        icon: 'success' })
+ }
+
 fetch('https://api.imgflip.com/get_memes') // llama a una api de memes randoms
     .then(response => response.json())
     .then(data => {//me devuelve una array de memes
@@ -44,240 +59,144 @@ class Tareas{//Constructor con la funcion de crear un nueva tarea
     }
 }    
 
-let one ;
+
+
+function eliminarTarea(categoria, nombre){
+    let index; 
+    let clase = document.querySelector(`.${nombre}`)
+    
+    for(let i = 0; i < todasLasTareas[categoria].length; i++){
+          if(todasLasTareas[categoria][i].nombre == nombre){
+            index = i
+          }
+    }
+    todasLasTareas[categoria].splice(index, 1)
+    localStorage.removeItem(noBarrita(nombre))
+    clase.remove()
+    tareaRemovida(categoria,nombre)
+}
+
+function tareaCompleta(categoria, nombre){
+    let index; 
+    let clase = document.querySelector(`.${nombre}`)
+    
+    for(let i = 0; i < todasLasTareas[categoria].length; i++){
+          if(todasLasTareas[categoria][i].nombre == noBarrita(nombre)){
+            index = i
+          }
+    }
+    todasLasTareas[categoria][index].tiempoAFinalizar = 'Completo'
+
+        let dataStorege = JSON.parse(localStorage.getItem(`${noBarrita(nombre)}`))
+        dataStorege.tiempoAFinalizar = 'Completo'
+        localStorage.setItem(`${noBarrita(nombre)}`, JSON.stringify(dataStorege))
+
+    document.querySelector(`.${nombre}>p`).classList += " completo"    
+    location.reload()
+}
 
 
 function iterarTodasLasCategorias(cate){
+    if(cate !== undefined){
+        for (let categoria in todasLasTareas) {
+            categoria = cate
+            todasLasTareas[categoria].forEach(n=> {
+                let valor = ponerBarrita(n.nombre)
+                let ul = document.createElement('ul')
 
-    function eliminarTarea(categoria, nombre){
-        let noBarrita = (x)=>{
-            return x.split("-").join(" ")
+                    document.getElementById(`${categoria}`).appendChild(ul)
+
+                let li = document.createElement('li')
+
+                    ul.appendChild(li)
+
+                let button = document.createElement('button')
+                let span =  document.createElement('span')
+                let p =  document.createElement('p')
+                    li.classList += valor
+
+                    li.appendChild(p)
+                    li.appendChild(span)
+                    li.appendChild(button)
+
+                    p.innerHTML += n.nombre
+        
+                    button.innerHTML += 'X'
+                    button.id += `${valor}`
+        
+                    span.innerHTML += n.tiempoAFinalizar.split(",").splice(0,2).join(" ")
+                    span.classList += 'span'
+
+                    document.querySelector(`.${valor}`).addEventListener('click', () =>{
+                        tareaCompleta(`${categoria}`,valor)
+                    })
+                    document.getElementById(`${valor}`).addEventListener('click', () =>{
+                    eliminarTarea(`${categoria}`,valor)})
+                        if(n.tiempoAFinalizar === 'Completo'){
+                            p.classList += ' completo'
+                        }
+            })
+            break
         }
-        let index; 
-        //let elemento = document.getElementById(`${nombre}`)
-        let clase = document.querySelector(`.${nombre}`)
-        //let tiempiA = document.querySelector(`${(nombre + '1')}`)
-        let tareaRemovida = (cate,nom) =>{
-           swal({
-               title:`Tu tarea fue eliminada con exito`,
-               text: `La tarea ${noBarrita(nom)} fue eliminada de la categoria ${cate}`,
-               icon: 'success' })
+    }else{
+        for (let categoria in todasLasTareas) {
+            todasLasTareas[categoria].forEach(n=> {
+                let valor = ponerBarrita(n.nombre)
+                let ul = document.createElement('ul')
+
+                    document.getElementById(`${categoria}`).appendChild(ul)
+
+                let li = document.createElement('li')
+
+                    ul.appendChild(li)
+                let button = document.createElement('button')
+                let span =  document.createElement('span')
+                let p =  document.createElement('p')
+                    li.classList += valor
+        
+                    li.appendChild(p)
+                    li.appendChild(span)            
+                    li.appendChild(button)
+
+                    p.innerHTML += n.nombre
+        
+                    button.innerHTML += 'X'
+                    button.id += `${valor}`
+        
+                    span.innerHTML += n.tiempoAFinalizar.split(",").splice(0,2).join(" ")
+                    span.classList += 'span'
+
+                    document.querySelector(`.${valor}`).addEventListener('click', () =>{
+                        tareaCompleta(`${categoria}`,valor)
+                    })
+                    document.getElementById(`${valor}`).addEventListener('click', () =>{
+                    eliminarTarea(`${categoria}`,valor)})
+                        if(n.tiempoAFinalizar === 'Completo'){
+                            p.classList += ' completo'
+                        }
+            })
         }
-        for(let i = 0; i < todasLasTareas[categoria].length; i++){
-              if(todasLasTareas[categoria][i].nombre == nombre){
-                index = i
-              }
-        }
-        todasLasTareas[categoria].splice(index, 1)
-        localStorage.removeItem(noBarrita(nombre))
-        clase.remove()
-        tareaRemovida(categoria,nombre)
-   }
-
-    let Compras =()=>{//Me generan una lista dependiendo los objetos o tareas que se encuentran dentro de la categoria seleccionada
-        let Compras = todasLasTareas["Compras"]
-        
-         Compras.forEach(n=>{
-            let ponerBarrita = (nombre)=>{
-                return  nombre.split(" ").join("-")
-            } 
-            let valor = ponerBarrita(n.nombre)
-
-            let ul = document.createElement('ul')
-            document.getElementById('compras').appendChild(ul)
-            let li = document.createElement('li')
-            ul.appendChild(li)
-            let button = document.createElement('button')
-            let span =  document.createElement('span')
-            li.innerHTML += n.nombre
-            li.classList += valor
-
-            li.appendChild(button)
-            li.appendChild(span)
-
-            button.innerHTML += 'X'
-            button.id += `${valor}`
-
-            span.innerHTML += n.tiempoAFinalizar.split(",").splice(0,2).join(" ")
-            document.getElementById(`${valor}`).addEventListener('click', () =>{
-                eliminarTarea("Compras",valor)})
-         })
-        }
-        
-    let Trabajo = () =>{
-            let Trabajo = todasLasTareas["Trabajo"]
-        
-        Trabajo.forEach(n=>{
-            let ponerBarrita = (nombre)=>{
-                let per =  nombre.split(" ").join("-")
-                return per
-            } 
-            let valor = ponerBarrita(n.nombre)
-
-           let ul = document.createElement('ul')
-           document.getElementById('trabajo').appendChild(ul)
-           let li = document.createElement('li')
-           ul.appendChild(li)
-           let button = document.createElement('button')
-           let span =  document.createElement('span')
-           li.innerHTML += n.nombre
-           li.classList += valor
-
-           li.appendChild(button)
-           li.appendChild(span)
-
-           button.innerHTML += 'X'
-           button.id += `${valor}`
-
-           span.innerHTML += n.tiempoAFinalizar.split(",").splice(0,2).join(" ")
-           document.getElementById(`${valor}`).addEventListener('click', () =>{
-               eliminarTarea("Trabajo",valor)})
-        })
-        
-        }
-        
-    let Universidad= () =>{
-        let Universidad = todasLasTareas["Universidad"]
-        
-         Universidad.forEach(n=>{
-            let ponerBarrita = (nombre)=>{
-                let per =  nombre.split(" ").join("-")
-                return per
-            } 
-            let valor = ponerBarrita(n.nombre)
-
-            let ul = document.createElement('ul')
-            document.getElementById('universidad').appendChild(ul)
-            let li = document.createElement('li')
-            ul.appendChild(li)
-            let button = document.createElement('button')
-            let span =  document.createElement('span')
-            li.innerHTML += n.nombre
-            li.classList += valor
-
-            li.appendChild(button)
-            li.appendChild(span)
-
-            button.innerHTML += 'X'
-            button.id += `${valor}`
-
-            span.innerHTML += n.tiempoAFinalizar.split(",").splice(0,2).join(" ")
-            document.getElementById(`${valor}`).addEventListener('click', () =>{
-                eliminarTarea("Universidad",valor)})
-         })
-        }
-        
-    let Ocio = () =>{
-        let Ocio = todasLasTareas["Ocio"]
-        
-         Ocio.forEach(n=>{
-            let ponerBarrita = (nombre)=>{
-                let per =  nombre.split(" ").join("-")
-                return per
-            } 
-            let valor = ponerBarrita(n.nombre)
-
-            let ul = document.createElement('ul')
-            document.getElementById('ocio').appendChild(ul)
-            let li = document.createElement('li')
-            ul.appendChild(li)
-            let button = document.createElement('button')
-            let span =  document.createElement('span')
-            li.innerHTML += n.nombre
-            li.classList += valor
-
-            li.appendChild(button)
-            li.appendChild(span)
-
-            button.innerHTML += 'X'
-            button.id += `${valor}`
-
-            span.innerHTML += n.tiempoAFinalizar.split(",").splice(0,2).join(" ")
-            document.getElementById(`${valor}`).addEventListener('click', () =>{
-                eliminarTarea("Ocio",valor)})
-         })
-        }
-        
-    let Otro= ()=>{
-         let Otro = todasLasTareas["Otro"]
-        
-         Otro.forEach(n=>{
-            let ponerBarrita = (nombre)=>{
-                let per =  nombre.split(" ").join("-")
-                return per
-            } 
-            let valor = ponerBarrita(n.nombre)
-
-
-            let ul = document.createElement('ul')
-            document.getElementById('otro').appendChild(ul)
-            let li = document.createElement('li')
-            ul.appendChild(li)
-            let button = document.createElement('button')
-            let span =  document.createElement('span')
-            li.innerHTML += n.nombre
-            li.classList += valor
-
-            li.appendChild(button)
-            li.appendChild(span)
-
-            button.innerHTML += 'X'
-            button.id += `${valor}`
-
-            span.innerHTML += n.tiempoAFinalizar.split(",").splice(0,2).join(" ")
-            document.getElementById(`${valor}`).addEventListener('click', () =>{
-                eliminarTarea("Otro",valor)})
-         })
-        }
-
-    switch(cate){
-        case "Compras":
-            Compras()
-            break;
-        case "Trabajo":
-            Trabajo()
-            break;
-        case "Universidad":
-            Universidad()
-            break;
-        case "Ocio":
-            Ocio()
-            break;
-        case "Otro":
-            Otro()
-            break;
-        default :
-            Compras()
-            Trabajo()
-            Universidad()
-            Ocio()
-            Otro()
-            break;
     }
 }
 
 
 
+let one ;
 
 function generaNuevaTarea(){ //Me genera una nueva tarea con datos provenientes del DOM y de la interaccion del usuario con los inputs del HTML
     let tarea = tar.value
     let categoria = cate.value
     let fecha = DateTime.fromISO(document.getElementById("fechaParaFinalizar").valueAsDate.addDays(1).toISOString());
-     if(tarea === '' ||  fecha === undefined){ //Me cheekea si el usuario no escribio un nombre de tarea o no escogio una fecha para finalizar la tarea
-        alert("No marcate una fecha o no escribiste el nombre de la tarea, por favor rellene el form de forma correcta")
-         } else{
+        if(tarea === '' ||  fecha === undefined){ //Me cheekea si el usuario no escribio un nombre de tarea o no escogio una fecha para finalizar la tarea
+            alert("No marcate una fecha o no escribiste el nombre de la tarea, por favor rellene el form de forma correcta")
+            } else{
                 one = new Tareas(tarea,categoria,fecha)
-
-                todasLasTareas[categoria].shift()
-
+                todasLasTareas[categoria].shift()   
                 todasLasTareas[categoria].push(one)
-
                 localStorage.setItem(one.nombre, JSON.stringify(one))// Añade las nuevas tares a localstorage y utiliza JSON stringify para pasar el objeto a JSON.
                 one.tuTareaFueAñadida();
-
-                iterarTodasLasCategorias(categoria)
-                   
-                }         
+                iterarTodasLasCategorias(categoria)   
+            }         
 }
 
 
